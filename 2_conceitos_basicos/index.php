cdd8ebc7c3c6bd47793733a5032f1fdca554400e
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-15 23:46:19 -0300

    Curso php conceitos basicos

--- 2_conceitos_basicos/index.php
+++ 2_conceitos_basicos/index.php
@@ -1,30 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conceitos basico -PHP</title>
     <style>
-        body{
+        body {
             background: aquamarine;
             text-align: center;
         }
     </style>
 </head>
 
-    <body>  
-        <?php
-            echo 'Hello World';
-            
-            echo '<hr>';
+<body>
 
-            $nome = 'Filipe';
-            echo $nome;
+    <?php
+    echo 'Hello World';
+
+    echo '<hr>';
+
+    $nome = 'Filipe';
+    echo $nome;
+
+    echo "<hr>";
+
+    if (5 > 3) {
+        echo "Dentro do if";
+    }
+    ?>
+
+    <?php
 
 
 
-        ?>
-    </body>
+
+
+    ?>
+
+
+</body>
 
 </html>
