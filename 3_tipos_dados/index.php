7b73cb61142472db51bda53e326b4edcb742a001
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-15 23:52:56 -0300

    Curso php tipos de dados

---
+++ 3_tipos_dados/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos de dados - PHP</title>
+    <style>
+        body{
+            background-color: aquamarine;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+        <h3>Tipos de dados</h3>
+        <hr>
+        <?php
+
+
+
+        ?>
+    
+</body>
+
+</html>
