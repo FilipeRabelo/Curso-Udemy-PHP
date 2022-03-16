50c00a3772dc4ec2224999835dc9be8178cf837f
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-16 00:25:24 -0300

    Curso php checando num float

---
+++ 5_numero_float/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Checando numeros Floats - PHP</title>
+    <style>
+        body {
+            background-color: aquamarine;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <?php
+
+        $a = 1.12;
+
+        echo $a;
+        echo "<br>";
+        echo 1.123;
+        echo "<br>";
+        echo 12.5 + 1.3278;
+        echo "<br>";
+        echo 12 + 1.3278;
+
+
+
+
+    ?>
+    
+</body>
+</html>
