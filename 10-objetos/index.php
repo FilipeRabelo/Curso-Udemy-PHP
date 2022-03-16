325c37b81ee9fec8bf83102984ded64c3f077682
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-16 12:31:23 -0300

    Curso php objetos POO

--- 10-objetos/index.php
+++ 10-objetos/index.php
@@ -1,24 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Orientacao a objetos - PHP</title>
     <style>
-        bocy{
+        body {
             background-color: aqua;
             text-align: center;
         }
     </style>
 </head>
+
 <body>
+
     <?php
-        
 
+        class Pessoa {   //Molde do objeto
 
+            // public $nome = " ";
+
+            function falar(){
+                echo "Ola pessoal";
+            }
+
+        }       
+
+        $filipe = new Pessoa();    //Novo objeto aprtir do molde (objeto) // Filho do objeto pai
+
+        $filipe -> nome = "Filipe";  //Adicionando propriedades ao novo objeto Pessoa
+
+        echo $filipe -> nome;
+
+        echo "<br>";
+
+        $filipe -> falar();
 
     ?>
-    
+
 </body>
+
 </html>
