03815e30b6083eb344ab4f941f5e934e520da833
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-15 22:22:39 -0300

    Curso php udemy intro

--- 1-intro/index.php
+++ 1-intro/index.php
@@ -1,5 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio - PHP</title>
+</head>
+<body>
+    <?php
+        $nome = 'Filipe';
+        $sobre_nome = 'Rabelo';
 
-    echo"Hello world";
+        echo $nome . $sobre_nome;
 
-?>
+    ?>
+</body>
+</html>
