7b11bf49e5febec05608697e14a749427062a826
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 12:15:46 -0300

    variaveis static

--- 4_variaveis/6-escopo_static/index.php
+++ 4_variaveis/6-escopo_static/index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -16,10 +17,43 @@
 </head>
 
 <body>
-
+        <h3>variavel static</h3>
         <?php
             
+            function teste(){
 
+                $a = 0;
+                $a++;
+
+                echo "$a <br> ";
+
+            }
+
+            teste();
+            teste();
+            teste();
+            teste();
+            teste();
+            teste();
+
+            echo "<hr>";
+
+            function teste_static(){
+
+                static $a = 0;
+                $a++;
+
+                echo "$a <br>";
+
+            }
+
+            teste_static();
+            teste_static();
+            teste_static();
+            teste_static();
+
+           
+            
 
 
         ?>
