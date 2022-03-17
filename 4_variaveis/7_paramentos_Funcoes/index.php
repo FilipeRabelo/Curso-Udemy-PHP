fb364b32f8d4f39f441683e424172bfb08624494
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 12:19:10 -0300

    Prametros funcoes

---
+++ 4_variaveis/7_paramentos_Funcoes/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>parametros funcoes - PHP</title>
+    <style>
+        body{
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h3>Paramentros funcoes</h3>
+    <?php
+        
+
+
+
+
+    ?>
+</body>
+
+</html>
