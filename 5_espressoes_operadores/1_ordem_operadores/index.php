5fc670d8b3eb28320c737b55b5b685a4c80eb9dd
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 13:18:31 -0300

    expressoes e operadores

--- 5_espressoes_operadores/1_ordem_operadores/index.php
+++ 5_espressoes_operadores/1_ordem_operadores/index.php
@@ -14,6 +14,54 @@
 </head>
 
 <body>
+
+        <?php
+
+            echo 2 + 3 * 5;
+            echo "<hr>";
+            echo (2 + 3) * 5;
+            echo "<hr>";
+            echo 5 + 2 /10;
+            echo "<hr>";
+            echo (5 + 2) / 10;
+            echo "<hr>";
+
+            $a = 5;
+            $b =2;
+            $c = 10;
+
+            echo $a + $b / $c;
+            echo "<hr>";
+            echo $c + $a / $b;
+            echo "<hr>";
+
+            $d = $a * $b * $c;
+            echo $d;
+
+            echo "<hr>";
+
+            $d = $a * ($b - $c);
+            echo $d;
+
+            echo "<hr>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        ?>
     
 </body>
 
