1af5603d67ae21566dc42423a83955e7eae359b2
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 14:55:15 -0300

    expressoes e operadores

---
+++ 5_espressoes_operadores/2_gettype()/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ordem operadores - PHP</title>
+    <style>
+        body{
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+        <?php
+
+        $operacao = "5" * 12;
+        
+        echo $operacao . "<hr>";
+
+        echo gettype($operacao) . "<hr>";
+
+        $b = "Filip";
+
+        echo gettype($b);
+
+
+       
+
+
+        ?>
+    
+</body>
+
+</html>
---
+++ 5_espressoes_operadores/3_operador_aritmeticos/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Operadores aritmeticos</title>
+    </head>
+
+    <body>
+
+        <?php
+
+            $soma = 5 + 5;
+            echo $soma . "<hr>";
+
+            $subt = 10 - 2;
+            echo $subt. "<hr>";
+
+            $multi = 2 * 199;
+            echo $multi . "<hr>";
+
+            $divi = 123 / 48;
+            echo $divi;
+
+        ?>
+        
+    </body>
+
+</html>
