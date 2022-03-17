02f9f8aa5128ef35c3175c0e469f93388434b2ab
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 15:57:57 -0300

    incre decre

--- 5_espressoes_operadores/7_incremento_decremento/index.php
+++ 5_espressoes_operadores/7_incremento_decremento/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>incremento e decremento</title>
     <style>
-        body{
+        body {
             background-color: aqua;
             text-align: center;
         }
@@ -15,19 +16,36 @@
 
 <body>
     <?php
-       
-       $x = 0;
-       $y = 0;
 
-       echo $x++;
-       echo "<br>";
+    $x = 0;
+    $y = 10;
 
-       echo $x--;
-       echo "<br>";
-       
-      
+    echo $x++;
+    echo "<br>";
+
+    echo $x--;
+    echo "<br>";
+
+    while ($x <= 10) {
+
+        echo $x;
+        $x++;
+        
+    }
+
+    echo "<br>";
+
+    do {
+
+        echo $y;
+        $y--;
+
+    } while ($y >= 0);
+
+
 
 
     ?>
 </body>
+
 </html>
