c5e6d173e7dfead6a13c21a471b7358ea430690d
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 20:51:28 -0300

    logico

--- 5_espressoes_operadores/11_oper_logico/index.php
+++ 5_espressoes_operadores/11_oper_logico/index.php
@@ -19,12 +19,71 @@
 
     <?php
 
-        if(5 > 2 && 10 < 100){
-            echo "true";
+        if(5 < 2 && 10 < 100){
+            echo "True";
         }else{
             echo "False";
         }
 
+        echo "<hr>";
+
+        if(5 > 2 && 10 < 100){
+            echo "True";
+        }else{
+            echo "False";
+        }
+
+        echo "<hr>";
+
+        if(5 < 2 || 2 < 5){
+            echo "Ok";
+        }
+
+        echo "<hr>";
+
+        if (!(5 > 2)) {
+            echo "ok";
+        }else{
+            echo "nao 2";
+        }
+        echo "<hr>";
+        if (!(50 < 2)) {
+            echo "ok";
+        }else{
+            echo "nao 2";
+        }
+
+        echo "<hr>";
+
+        /////////////////////////////
+
+       echo "operador cast <br>";
+
+       $a = (string) 34;  // converte os tipos de dados // float, string, int //
+      
+       if($a === "34"){
+           echo "34 é string <hr>";
+       }
+
+       $b = (int) 12.9;
+       
+       if($b === 12){
+           echo "conversao 1 ok <br>";
+       }
+
+       $c = (int) true;
+
+       if($c === 1){
+           echo "conversao 2 ok <br>";
+       }
+
+       $d = (int) [1, 2, 3];
+
+    //    if($d === 1.0, 2.0, 3.0){  nao dá
+    //        echo "consersao 3 ok <br>";
+    //    }
+
+       $e = (int) "testando"; // nao dá
 
     ?>
 </body>
