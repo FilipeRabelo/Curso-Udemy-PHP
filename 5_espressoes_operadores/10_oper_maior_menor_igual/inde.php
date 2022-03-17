4d8c47ed06c151414d315e403b3d8d1d943360ed
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 18:59:13 -0300

    oper < > =

---
+++ 5_espressoes_operadores/10_oper_maior_menor_igual/inde.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operador comparacao</title>
+    <style>
+        body {
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h3>Operadores identico / nao identico</h3>
+
+    <?php
+        
+       
+
+
+
+    ?>
+</body>
+
+</html>
