3937ea04ff625b9b324f7bcbc5fa1c57a32ed708
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 11:49:14 -0300

    variaveis escopo globo

--- 4_variaveis/5_escopo_global/index.php
+++ 4_variaveis/5_escopo_global/index.php
@@ -16,10 +16,51 @@
 
 <body>
 
-    
     <?php
-        echo "Escopo global - Fora da Funcao";
-            
+        echo "Escopo global - Fora da Funcao";        
+        echo "<hr>";
+
+
+        $teste = "asd"; // 1
+
+        echo "$teste global 1  <hr>";
+
+        if (5 > 2) {
+
+            $teste = "dsa 000"; //2
+
+            echo "$teste if <hr>";
+        }
+
+        echo "$teste global 1";
+        echo "<hr>";
+
+        $teste = "xxx";  //3
+
+        echo "$teste global 2 <hr>";
+
+        function funcao(){
+
+            $teste = "xsxsxs";
+
+            echo "$teste Escopo da funcao";
+
+        }
+
+        funcao();
+
+        echo "<hr>";
+
+        function testando_global(){
+
+            global $teste;
+
+            echo "$teste global funcao";
+
+        }
+
+        testando_global();
+
 
     ?>
 
