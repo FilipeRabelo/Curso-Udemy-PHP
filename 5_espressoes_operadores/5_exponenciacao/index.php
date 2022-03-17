e1abd5efb1a1e25df60e368f7df2cb5528820903
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-17 15:22:13 -0300

    expressoes e operadores

---
+++ 5_espressoes_operadores/5_exponenciacao/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exponenciacao</title>
+    <style>
+        body{
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+        echo "Exponenciacao";
+        echo "<hr>";
+
+        echo 5 ** 2;
+        echo "<hr>";
+
+        echo 5 ** 3;
+        echo "<hr>";
+
+        echo 215 ** 6585;
+        echo "<hr>";
+
+        echo 215 ** 110;
+        echo "<hr>";
+
+
+
+    ?>
+</body>
+</html>
