c3108c923648975e4f165f2fbd4ca9592eb4af82
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-18 18:07:19 -0300

    break

--- 7_estrutura_repeticao/2_brake/index.php
+++ 7_estrutura_repeticao/2_brake/index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Estrutura repeticao While - PHP</title>
     <style>
-        body{
+        body {
             background-color: aquamarine;
             text-align: center;
         }
@@ -14,18 +15,37 @@
 </head>
 
 <body>
-        <h3>Estrutura repeticao While - PHP</h3>
-        <?php
+    <h3>Estrutura repeticao While - PHP</h3>
+    <?php
 
-            echo "Break <br><br>";
+    echo "Break <br><br>";
 
-           
-            
+    $x = 0;
+    
+    while ($x < 10) {
+
+        echo "O X é o $x <br>";
+
+        if($x === 5 ){  //qndo o x for = a 5 ele sai do loop com o break
+
+            echo "Terminando o Loop";
+
+            break;  // para sair do loop
+
+        }
+
+        $x++;
+         
+    }
+
+    echo "saiu do loop";
 
 
 
 
 
-        ?>
+
+    ?>
 </body>
+
 </html>
