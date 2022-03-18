a2b096d085e9ec3e7c6a291902e00cc2dfdefc06
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-18 10:27:43 -0300

    Estrutura switch

---
+++ 6_estrutura_controle/4_switch/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura switch- PHP</title>
+    <style>
+        body {
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h3>Estrutura switch</h3>
+    <?php
+
+    
+
+
+    ?>
+
+</body>
+
+</html>
