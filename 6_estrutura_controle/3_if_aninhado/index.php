842f5a96cbf4450db90eb59ecb8b5c6c609bfd6d
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-18 00:49:49 -0300

    estrutura if aninhado

---
+++ 6_estrutura_controle/3_if_aninhado/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura if aninhado - PHP</title>
+    <style>
+        body {
+            background-color: aqua;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+        <h3>Estrutura if aninhado</h3>
+    <?php
+
+        
+      
+
+    ?>
+
+</body>
+
+</html>
