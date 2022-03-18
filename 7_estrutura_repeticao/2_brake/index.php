db83410f4f13b0824d1b856282cc6c10f6f11591
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-18 17:54:09 -0300

    break

---
+++ 7_estrutura_repeticao/2_brake/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura repeticao While - PHP</title>
+    <style>
+        body{
+            background-color: aquamarine;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+        <h3>Estrutura repeticao While - PHP</h3>
+        <?php
+
+            echo "Break <br><br>";
+
+           
+            
+
+
+
+
+
+        ?>
+</body>
+</html>
