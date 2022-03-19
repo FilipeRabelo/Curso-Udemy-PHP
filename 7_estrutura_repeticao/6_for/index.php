37d7bbe45767c8ad7c2e3044e22e12d416d188ee
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-19 00:03:50 -0300

    For/break/continue/aninhado

--- 7_estrutura_repeticao/6_for/index.php
+++ 7_estrutura_repeticao/6_for/index.php
@@ -21,64 +21,132 @@
 
     <?php
 
+    //EXERCICIOS
     //contador ; condicao ; incremento/decremento//
+
+    $arr = [
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20
+    ];
+
+    for ($i = 0; $i < count($arr); $i++) {
+
+        if ($arr[$i] % 2 == 0) {
+
+            echo "Número: $arr[$i]. <br>";
+        }
+    }
+    echo "<hr>";
+
+    //EXERCICIOS
+    //Criando o array com um metado//
+
+    $arr = [];
+
+    for ($i = 1; $i <= 10; $i++) {
+
+        array_push($arr, $i);  //argumento , elemento
+
+    }
+
+    print_r($arr);
+
+
+
+
+
+    //EXERCICIOS
+    echo "<hr>";
+
+    $arr_2 = [];
+
+    for ($x = 10; $x <= 20; $x++) {
+
+        array_push($arr_2, $x);
+    }
+
+    print_r($arr_2);
+    
+    echo "<br>";
+
+    for ($x = 0; $x < count($arr_2); $x++) {
+
+        if ($arr_2[$x] % 2 != 1) {
+
+            echo " Numero impa $arr_2[$x] <br>";
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+    echo "<hr>";
 
     $nome = "Filipe";
 
-    for($n = 0; $n <= 10; $n++){
+    for ($n = 0; $n <= 10; $n++) {
 
         echo "Testando com if de nome de variavel $n <br>";
 
-        if($n == 5){
+        if ($n == 5) {
             echo "$nome <br>";
         }
-
     }
+
 
     echo "<hr>";
 
-    for($x = 0; $x <= 10; $x++){
+
+    for ($x = 0; $x <= 10; $x++) {
 
         echo "Contador $x <br>";
 
-        if($x == 5 || $x == 7){
+        if ($x == 5 || $x == 7) {
 
             $x++;
-            
-            continue;
-            
-        }
 
+            continue;
+        }
     }
+
 
     echo "<hr>";
 
-    for($i = 10; $i >= 0; $i--){
+
+    for ($i = 10; $i >= 0; $i--) {
 
         echo "Contador $i <br>";
 
-        if($i == 5){
+        if ($i == 5) {
 
             $x--;
-            
+
             break;
         }
-
     }
 
+
     echo "<hr>";
+
 
 
     // For Aninhado
 
-    for($y = 0; $y <= 10; $y++){  
+    for ($y = 0; $y <= 10; $y++) {
 
         echo "<br>Primeiro for '$y' <br><br>";
 
-        for($z = 0; $z <= 5; $z++){
+        for ($z = 0; $z <= 5; $z++) {
             echo "Segundo for $z <br>";
         }
-
     }
 
 
