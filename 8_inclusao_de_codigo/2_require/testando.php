d3960ecb4a6caf8c90ddeed3385db9a44480d856
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-19 18:52:20 -0300

    includ/require

--- 8_inclusao_de_codigo/2_require/testando.php
+++ 8_inclusao_de_codigo/2_require/testando.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
     echo "TESTANDO VOLTAR PASTA <br>";
-    ?>
+
--- 8_inclusao_de_codigo/2_require/teste.php
+++ 8_inclusao_de_codigo/2_require/teste.php
@@ -1,17 +1,6 @@
 <div>
 
     <h1>Encontre os melhores produtos</h1>
-    <p>Caneta bic R$1,00</p>
-
-    <ol>
-        <li>
-            <a href="#">Home</a>            
-        </li>
-        <li>
-            <a href="#">Home</a>            
-        </li>       
-    </ol>
-
-  
+    <p>Caneta bic R$1,00</p>  
 
 </div>
