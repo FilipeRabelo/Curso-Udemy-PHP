716fb7abe2d4821b65a1e199db267b3cf3666c8a
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-18 21:00:59 -0300

    loop aninhado

--- 7_estrutura_repeticao/3_loop_aninhado/index.php
+++ 7_estrutura_repeticao/3_loop_aninhado/index.php
@@ -15,10 +15,46 @@
 </head>
 
 <body>
+
     <h3>Loop aninhado- PHP</h3>
+
     <?php
 
-        
+        $i = 1; // PRIMEIRO CONTADOR
+
+        while($i <= 10){
+
+            echo "Loop Externo $i <br>";  // PARA CADA 1 LOOP ESTERNO ELE COMPLETA O INTERNO DEPOIS VOLTA PARA O EXTERNO
+
+            $j = 1; // SEGUNDO CONTADOR
+
+            echo "<hr>";
+
+            while($j <= 5){
+
+                echo "Loop interno $j <br>";
+
+                $j ++;
+
+            }
+
+            $xxx = 1;
+
+            // DA PARA ANINHAR MAIS LOOPS ANINHADOS DENTRO DA ESTRUTURA
+
+            while($xxx <= 5 ){
+
+                echo "Loop interno 2 $xxx <br>";
+
+                $xxx ++;
+
+            }
+
+            $i ++;
+
+            echo "<hr>";
+
+        }
 
 
 
