c3bc7ede2f67eedb369f60b2f498e6b4d5b79510
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 01:57:44 -0300

    funções/return/exercicio

--- 9_funções/5_return/index.php
+++ 9_funções/5_return/index.php
@@ -40,25 +40,8 @@
     echo $z;
 
     echo "<hr>";
-    //exercicio
-
-    function num($a){
-        
-        // return $a ** 2;
-
-        $resultado = $a ** 2;
-        return $resultado;
 
 
-    }
 
-    echo num(5) . "<br>";
-    echo num(2) . "<br>";
-    echo num(10) . "<br>";
-
-    $z = num(5);
-    echo $z . "<br>";
-
-    $xxx = num($z + 55);
-    echo $xxx;
+   
 
