58bf3a2ac0f77e796713be11f668252a840c3b47
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 18:27:05 -0300

    strings

--- 10_strings/1_Interpolação_variáveis/index.php
+++ 10_strings/1_Interpolação_variáveis/index.php
@@ -1,3 +1,15 @@
 <?php
 
-    echo "Ola mundo";
+    $saudacao = "Ola mundo";
+    
+    $nome = "Filipe";
+
+    $idade = 32;
+
+    $profissao = "Programador";
+
+    echo "Eu tenho $idade anos! Sem { } <hr>";
+
+    echo "Eu tenho {$idade} anos! com  { } <hr>";
+
+    echo "$saudacao, eu sou o $nome e tenho $idade anos e sou $profissao!";
