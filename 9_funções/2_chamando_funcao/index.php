bcaf260f4885d7e6407ff9ac91746c287077d464
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-19 22:56:21 -0300

    funções

---
+++ 9_funções/2_chamando_funcao/index.php
@@ -0,0 +1,20 @@
+<?php
+
+    function soma($a, $b){
+
+        echo $a + $b;
+
+    }
+
+    echo soma(2.545, 4154);
+
+    echo "<br>";
+
+
+    function soma2(){
+
+        echo 4 + 2;
+
+    }
+
+    echo soma2();
