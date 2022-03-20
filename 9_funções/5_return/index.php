861067c8f20fe662fc473a33ec048816afb48679
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 01:50:51 -0300

    funções/return/exercicio

--- 9_funções/5_return/index.php
+++ 9_funções/5_return/index.php
@@ -22,9 +22,33 @@
 
     echo soma(4, 4) . "<br>";
 
-    $x = soma(2, 4);
+    $x = soma(2, 4);  // Colocar a funcao numa variavel para ser melhor utilizada//
     echo $x . "<br>";
 
-    $y = soma($x, 19);
+    $y = soma($x, 19); // utilizar  a variavel para criar outra variavel//
     echo $y . "<br>";
+
+
+    function teste_retorno(){
+
+        return "Testando";
+
+    }
+
+    $z = teste_retorno();
+
+    echo $z;
+
+    echo "<hr>";
+    //exercicio
+
+    function num($a){
+
+        return $a ** 2;
+
+    }
+
+    echo num(5) . "<br>";
+    echo num(2) . "<br>";
+    echo num(10) . "<br>";
 
