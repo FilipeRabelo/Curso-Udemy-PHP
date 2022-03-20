ddb2acb26067e2e639f453b2a092e25a502e6712
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 02:08:57 -0300

    funcoes/escopo

--- 9_funções/7_escopo/index.php
+++ 9_funções/7_escopo/index.php
@@ -1,3 +1,18 @@
 <?php
 
+    // ESCOPO LOCAL
     
+    $a = 10; //ESCOPO GLOBAL DE A
+
+    function teste_escopo(){
+
+        $a = 5;  // ESCOPO LOCAL DE A
+
+       echo "ESCOPO LOCAL DE A $a <br><br>";
+
+    }
+
+    echo "ESCOPO GLOBAL DE A $a <br><br>";
+    
+    teste_escopo($a);
+
