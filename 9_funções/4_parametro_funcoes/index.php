86be0b6cf5b1787611f49304960dc1b9b935dd3f
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 00:43:18 -0300

    funções/parametros

--- 9_funções/4_parametro_funcoes/index.php
+++ 9_funções/4_parametro_funcoes/index.php
@@ -1,3 +1,28 @@
+
+
+
+
+
 <?php
 
-    
+    function velocidade_maxima($vel){
+
+        if(is_int($vel)){
+            echo "O carro atingiu a velocidade maxima de $vel km/h <br>";
+        }else{
+            echo "Por favor, passe um número inteiro!";
+        }
+
+    }
+
+    velocidade_maxima(200);
+    velocidade_maxima(210);
+    velocidade_maxima(100);
+
+    $velocidade = 125;
+    velocidade_maxima($velocidade);
+
+    velocidade_maxima("teste");
+
+
+
