c777a446829efe918c4b3c77db25cf09b3b04546
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 18:41:46 -0300

    strings

---
+++ 10_strings/2_valores_escape/index.php
@@ -0,0 +1,24 @@
+<?php
+
+    // dizendo para o html que o arquivo é texto puro
+    header("content-type: text/plain"); 
+
+    
+    // Pular linha
+
+    echo "Isso aqui vai ficar na primeira linha \nE isso na segunda linha \n";
+
+   
+    // Tab
+
+    echo "Testando o tab \t aqui \n";
+
+
+    // Barra invertida
+
+    echo "Barra invertida \\ \n";
+
+
+    // Dólar $
+
+    echo "imprimindo o dólar \$teste";
