d304a663a2a09306a39ee05ca11d135c67f37b08
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-19 23:14:23 -0300

    funções

--- 9_funções/2_chamando_funcao/index.php
+++ 9_funções/2_chamando_funcao/index.php
@@ -6,9 +6,7 @@
 
     }
 
-    echo soma(2.545, 4154);
-
-    echo "<br>";
+    echo soma(2.545, 4.154) . "<br>";
 
 
     function soma2(){
@@ -17,4 +15,23 @@
 
     }
 
+    echo soma2() . "<br>";
+
+    function soma3(){
+
+        $a = 5;
+        $b = 4;
+
+        echo $a + $b . "<br>";
+
+    }
+
+    echo soma3();
     echo soma2();
+
+    echo "<hr>";
+    // FUNCAO DO PHP PARA DEIXAR AS LETRAS MAIUSCULAS //
+
+    echo strtoupper("testando funÇÃo");
+    
+   
