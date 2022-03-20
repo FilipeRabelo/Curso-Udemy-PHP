85b5994e1eba2083cda9083fedb218411c15879e
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 02:14:52 -0300

    funcoes/escopo

--- 9_funções/7_escopo/index.php
+++ 9_funções/7_escopo/index.php
@@ -4,15 +4,29 @@
     
     $a = 10; //ESCOPO GLOBAL DE A
 
+    $b = 15;
+
     function teste_escopo(){
 
         $a = 5;  // ESCOPO LOCAL DE A
+        
+        $a++;
 
-       echo "ESCOPO LOCAL DE A $a <br><br>";
+        global $b; //uTILIZANDO A VARIAVEL GLOBAL DENTRO DA FUNCAO COM GLOBAL
+       
+        $b++; // alterando o B
+
+       echo "ESCOPO LOCAL DE DENTRO DA FUNCAO  A $a <br><br>";
+
+       echo "ESCOPO GLOBAL DENTRO FUNCAO DE B $b <br><br>";
 
     }
 
     echo "ESCOPO GLOBAL DE A $a <br><br>";
     
+    echo "ESCOPO GLOBAL DE B $b <br><br>";
+    
     teste_escopo($a);
+
+    echo "ESCOPO GLOBAL DE B ALTERADO DENRO DA FUNCAO $b <br><br>";
 
