3216f9ecd25127df3fd0c4554a91397edff02d25
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 13:00:09 -0300

    arrays

---
+++ 11-arrays/6_array_multidimensioanal/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    echo "Quando inserimos arrays dentro de arrays formamos um array multidimensional
+        tambem conhecidos como matriz<br>
+        Para acessar esse tipo de array tambem utilizamos indices, 
+        acessando o externo e dpois o interno.<br>
+        exemlpo: $ arr[1][0] => primeiro elemento do segndo array<hr>";
+
