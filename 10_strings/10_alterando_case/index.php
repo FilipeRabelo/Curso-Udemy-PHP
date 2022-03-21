baeb3c223bc02a7d69f5a05b7c0c3886fda211b5
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 22:20:07 -0300

    strings

--- 10_strings/10_alterando_case/index.php
+++ 10_strings/10_alterando_case/index.php
@@ -1,3 +1,10 @@
 <?php
 
-    
+    echo "Podemos alterar as strings para 
+            maisculas ou minúsculas com funcoes de php <hr>";
+
+    echo "strtolower - Todas as letras para minúsculas <br>";
+    echo "strtoupper - Todas as letras para MAIÚSCULAS";
+
+    echo "<hr>";
+
