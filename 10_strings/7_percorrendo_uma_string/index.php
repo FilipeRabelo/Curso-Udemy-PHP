c59daf9c69f91e48392476f25fe274125c9e00a9
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 21:28:17 -0300

    strings

--- 10_strings/7_percorrendo_uma_string/index.php
+++ 10_strings/7_percorrendo_uma_string/index.php
@@ -1,3 +1,13 @@
 <?php
 
-    
+    echo "Podemos percorrer cada um dos caracteres de uma string <br>
+    Para isso vamos utilizar uma estrutura de repeticao<br>
+    E o metado strlen para saber o numero de caracteres atraves do $ i <hr>";
+
+    $str = "Esta e uma string muito grande, ela tem varios caracteres";
+
+    for($i = 0; $i < strlen($str); $i ++){
+
+        echo "$str[$i] <br>";
+
+    }
