6809be0503f2fa65d3342120f0e2a2398165bb8b
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 23:10:32 -0300

    strings

--- 10_strings/14_resgatendo_string/index.php
+++ 10_strings/14_resgatendo_string/index.php
@@ -1,3 +1,12 @@
 <?php
 
-    
+    echo "Com a funcao 
+    substr,
+    podemos resgatar apenas uma parte de string<hr>";
+    echo "Ex: substr (str, inicio, fim) <hr>";
+
+    $str = "Esta é a minha string";
+
+    $minha = substr($str, 10, 5); // STRING PAI. INDICE INICIAL, COMPRIMENTO DA PALAVRA
+
+    echo $minha;
