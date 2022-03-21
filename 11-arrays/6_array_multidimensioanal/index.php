32bc31c8c120f6f8864ecb2877e9705f64bf3eb1
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 13:08:18 -0300

    arrays

--- 11-arrays/6_array_multidimensioanal/index.php
+++ 11-arrays/6_array_multidimensioanal/index.php
@@ -6,3 +6,14 @@
         acessando o externo e dpois o interno.<br>
         exemlpo: $ arr[1][0] => primeiro elemento do segndo array<hr>";
 
+        $arr = [
+            [1, "array0", 3],  // array [0] e seus elementos
+            [2, 4, "array1"]   // array [1] e seus elementos
+        ];
+
+        print_r($arr);
+        echo "<hr>";
+
+        echo $arr[0][1];
+        echo "<br>";
+        echo $arr[1][2];
