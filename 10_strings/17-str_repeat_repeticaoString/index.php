6e7220dc6d5da6806681c0c69637c290f8c6ccd1
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 23:55:39 -0300

    strings

--- 10_strings/17-str_repeat_repeticaoString/index.php
+++ 10_strings/17-str_repeat_repeticaoString/index.php
@@ -6,13 +6,15 @@
 
     $frase = "Eu sou programador backend <br>";
 
-    echo str_repeat($frase, 2);
+    $string_repetida=  str_repeat($frase, 25);
 
-    $x = 0;
-    $y = 31;
+    echo $string_repetida;
 
-   if($x == 10 || $y > 32){
-       echo str_repeat($frase, 20);
-   }else{
-       echo str_repeat($frase, 10);
-   }
+//     $x = 0;
+//     $y = 31;
+
+//    if($x == 10 || $y > 32){
+//        echo str_repeat($frase, 20);
+//    }else{
+//        echo str_repeat($frase, 10);
+//    }
