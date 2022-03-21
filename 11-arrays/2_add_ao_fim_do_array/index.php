f80772aa5e19ab3507e50022b3968b1d99a1b0c5
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 11:57:31 -0300

    arrays

--- 11-arrays/2_add_ao_fim_do_array/index.php
+++ 11-arrays/2_add_ao_fim_do_array/index.php
@@ -1,6 +1,12 @@
 <?php
 
     echo "Podemos add um valor ao fim de um array utilizando a atribuicao sem determinar um indice<br>
-    Entao o valor atribuido sera enviado para o ultimo e novo indice do array;";
+    Entao o valor atribuido sera enviado para o ultimo e novo indice do array;<br>";
 
-    
+    echo "Exemplo => $ arr[] = 5 <hr>";
+
+    $arr = [1,2,3,4];
+
+    $arr[] = 5;
+
+    print_r($arr);
