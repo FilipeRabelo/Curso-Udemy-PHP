4b779e617ea51e2ef095a5f89aab657824ddbc3b
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 23:42:34 -0300

    strings

--- 10_strings/16_strrev_revertendo_string/index.php
+++ 10_strings/16_strrev_revertendo_string/index.php
@@ -5,4 +5,8 @@
 
     $frase = "Eu sou programador bakend";
 
-    echo strrev($frase);
+    echo strrev($frase) . "<br>";
+
+    $frase_2 = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, deserunt.";
+
+    echo strrev($frase_2);
