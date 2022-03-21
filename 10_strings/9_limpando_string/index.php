6d253b2c5a6d9dde72a7239b1c4d897e83e5b17b
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 22:15:50 -0300

    strings

--- 10_strings/9_limpando_string/index.php
+++ 10_strings/9_limpando_string/index.php
@@ -2,6 +2,16 @@
 
     echo "Podemos remover os espacos em branco de uma string com funcoes
     de php, desta forma conseguimos remover os espacos desnecessarios inseridos pelos
-    usuarios...";
+    usuarios...<hr>";
+
+    $str1 = "            Filipe           ";
+
+    echo "Esta é a string 1: $str1. <br>";
+
+    $str1_limpa = trim($str1);
+
+    echo "Esta é a string 1: $str1_limpa. <br>";
+
+    echo "<hr>";
 
     
