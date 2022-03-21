5804881e4367018731129595f91c258602b1806c
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 10:13:45 -0300

    strings

--- 10_strings/24_strstr_resto_string/index.php
+++ 10_strings/24_strstr_resto_string/index.php
@@ -2,4 +2,9 @@
 
     echo "Com a funcao strstr podemos encontrar um texto em uma string";
 
-    echo "se ele encontrar a palavra em especifica ele retorna toda a string";
+    echo "se ele encontrar a palavra em especifica ele retorna toda a string <hr>";
+
+    $str = "Testando o resto da string pra ver se dar certo";
+
+    $rest = strstr($str, "resto");
+    echo $rest;
