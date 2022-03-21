2a1c458c8263b07148009ee2108ca24d812a113c
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-20 22:54:06 -0300

    strings

--- 10_strings/13_tags_removendo_html/index.php
+++ 10_strings/13_tags_removendo_html/index.php
@@ -1,3 +1,7 @@
 <?php
 
-    
+    echo "Podemos remover as tags de HTML de uma string com a funcao: <br>
+    strip_tags; <br>
+    Geralmente para salvar dados no banco de daos removemos as tags!
+    ";
+
