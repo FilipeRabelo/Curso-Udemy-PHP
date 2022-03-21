e801d524ff23ede6d184eb4fae7fdf2863d95243
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 13:12:18 -0300

    arrays

--- 11-arrays/6_array_multidimensioanal/index.php
+++ 11-arrays/6_array_multidimensioanal/index.php
@@ -7,8 +7,8 @@
         exemlpo: $ arr[1][0] => primeiro elemento do segndo array<hr>";
 
         $arr = [
-            [1, "array0", 3],  // array [0] e seus elementos
-            [2, 4, "array1"]   // array [1] e seus elementos
+            [1, "array 0", 3],  // array [0] e seus elementos
+            [2, 4, "array 1"]   // array [1] e seus elementos
         ];
 
         print_r($arr);
@@ -17,3 +17,10 @@
         echo $arr[0][1]; //acessando primeiro array e segundo elemento//
         echo "<br>";
         echo $arr[1][2]; // acessando segundo array e 3 elemento//
+        echo "<br>";
+
+        echo count($arr);
+        echo "<br>";
+        echo count($arr[0]);
+        echo "<br>";
+        echo count($arr[1]);
