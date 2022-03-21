7217b7436af48d54d198012c73eaf3505eeb09da
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 12:55:50 -0300

    arrays

--- 11-arrays/5-count_elementos_numero/index.php
+++ 11-arrays/5-count_elementos_numero/index.php
@@ -1,6 +1,6 @@
 <?php
 
-    echo "Podemos obter o numero de elementos de um array com a funcao COUNT<br>
+    echo "Podemos obter o numero (QUATIDADE) de elementos de um array com a funcao COUNT<br>
     Basta passar o array com ARGUMENTO, Uminteiro sera retornado<hr>";
 
 
@@ -14,4 +14,20 @@
         $soma = $arr[$i] + 10;
 
         echo $soma . "<br>";
+    }
+
+    echo "<hr>";
+
+    $arr_2 = [
+        "nome" => "Filipe",
+        "idade" => 32,
+        "profissao" => "programador"
+    ];
+
+    echo count($arr_2) . "<hr>";
+
+    for($i = 0; $i < count($arr_2); $i++){
+
+        print_r($arr_2) . "<br>";
+
     }
