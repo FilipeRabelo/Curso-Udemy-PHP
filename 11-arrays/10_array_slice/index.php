7e35b24d869e2a4ecc51950d79c2fdbfe10bb39a
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-21 14:29:25 -0300

    arrays

---
+++ 11-arrays/10_array_slice/index.php
@@ -0,0 +1,13 @@
+<?php
+
+     echo "array_slice podemos resgatar uma faixa de elementos de um array<br>
+     Passamos 3 parametros: <br> 
+     O array <br>
+     Indice inicial <br>
+     Quantos elementos queremso resgatar a partir do indice <hr>";
+
+     $arr = [2, 4, 6, 8, 10, 16, 18, 14];
+
+     $slice_1 = array_slice($arr, 1, 3);
+
+     print_r($slice_1);
---
+++ 11-arrays/11_array_chunk/index.php
@@ -0,0 +1,5 @@
+<?php
+
+
+    echo "POdemos dividir um array grande.<br>
+    passamos o array como argumento e tambem o numero de elementos q cada array DEVE TER<hr>";
