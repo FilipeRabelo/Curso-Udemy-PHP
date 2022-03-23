0445ebf3444d32abe15de00ad9c01ff9a1182421
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-23 18:07:18 -0300

    classes

--- 12_Orientacao_objetos/2-instanciando_objetos/index.php
+++ 12_Orientacao_objetos/2-instanciando_objetos/index.php
@@ -1,7 +1,26 @@
 <?php
 
-    class Teste{
+    echo "Instanciar == Criar  ";   
 
-        // Propriedades => 
+    echo "Para instanciar um  objeto vamos utilizar a palavra NEW em conjunto do nome da classee;<br>
+    A partir dai uma entidade ou varias entidades com as caracteristicas da classe sera criada<br>
+    
+    Normalmente emcapsulamos esta valor em uma VARIAVEL;<br>
+    
+    Exemplo => $ filipe = new User  ";
+
+    class User{
+
+        // caracteristicas => Propriedades => Variaveis
+        // comportamentos => metados => funções 
 
     }
+
+    $filipe = new User;
+    $giulia = new User;
+    $rafael = new User;
+    $daniela = new User;
+
+
+
+
