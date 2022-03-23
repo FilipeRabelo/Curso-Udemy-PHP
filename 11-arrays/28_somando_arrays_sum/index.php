d371e41ebfd77c1892101a023b389f924a7ba945
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-23 13:27:40 -0300

    array

---
+++ 11-arrays/28_somando_arrays_sum/index.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo "Funcao array_sum para somar todos os elementos numeicos do array q passamos como argumento<hr>";
+
+    $arr = [2 ,4 ,5, 1.5 ,21, 14];
+
+    $soma = array_sum($arr);
+
+    print_r($soma);
+
+
+
+?>
