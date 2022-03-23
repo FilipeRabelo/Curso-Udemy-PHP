71f17c8a7b76359844aee10e634ec898a09dd0a8
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-23 17:41:24 -0300

    classes

---
+++ 12_Orientacao_objetos/0_o que sao/index.php
@@ -0,0 +1,25 @@
+
+        <h3>O que são objeots?</h3>
+<p>
+    Objetos sao entidades que possuem COMPORTAMENTOS e CARACTERISTICAS; <br>
+    As CARACTERISTICAS sao conhecidas como PROPRIEDADES => VARIAVEIS; <br>
+    Os COMPORTAMENTOS como METADOS => FUNÇÕES <br>
+    Os objetos interagem entre si e sistemas sao escritos orientados a objetos "POO" <br>
+    php desenvolvemos neste paradigma <br>
+</p>
+
+        <h3>O que são classes?</h3>
+<p>
+    Classes são os PAIS dos Objetos; <br>
+    Em PHP sempre que vamos criar ou INSTANCIAR um objeto vamos precisar de uma classebr
+    A classe contem o MOLDE Do OBJETO, ou seja, seus METADOS e suas PROPRIEDADES; <br>
+    Podemos mudar o valor para cada objeto criado, mas ele parte do que a classe impoem <br>
+</p>
+
+<?php
+
+
+
+
+
+?>
