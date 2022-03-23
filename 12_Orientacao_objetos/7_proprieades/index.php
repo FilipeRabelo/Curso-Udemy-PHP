fb190384c9bb5d47cee67b17d6cb2a4c096064a5
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-23 19:08:21 -0300

    classes

---
+++ 12_Orientacao_objetos/7_proprieades/index.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo "Declarar uma propriedades vamos criar uma
+    VARIAVEL dentro de uma CLASS<br>
+    Porem precisamos definir uma PRIVACIDADE, EX: PUBLIC<br>
+    UMa propriedade public pode ser acessada fora do escopo do objeto<br>
+    ex: public $ idade = 32";
+
+    
