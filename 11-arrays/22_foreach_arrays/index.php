339004900f18f57489ed9111c5cf3e37b8021036
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-23 10:33:16 -0300

    arrays

--- 11-arrays/22_foreach_arrays/index.php
+++ 11-arrays/22_foreach_arrays/index.php
@@ -1,3 +1,35 @@
 <?php
 
-    
+    echo "Foreach = Utilizamos a notação de chave  = > valor, 
+    temos acesso rapido tambem a arrys associativos<br>; 
+    Ex: foreach($ itens as $ key => $ value){}; <br>
+    chave = referencia;
+    <hr>";
+
+
+
+    $filipe = [
+        "nome" => "Filipe",
+        "idade" => 32,
+        "profissao" => "programador"
+    ];
+
+    $giulia = [
+        "nome" => "Giulia",
+        "idade" => 6,
+        "profissao" => "estudante"
+    ];
+
+    foreach($filipe as $caracteristicas => $value){   //$caracteristicas = $key //
+
+        echo "$caracteristicas => $value <br>";
+
+    }
+
+    echo "<hr>";
+
+    foreach($giulia as $caracteristicas => $value){
+
+        echo "$caracteristicas => $value <br>";
+
+    }
