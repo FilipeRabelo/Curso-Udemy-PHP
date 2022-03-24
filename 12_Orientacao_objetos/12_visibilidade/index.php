c1762ebb9cbb9aaa85aac2496b6329ac3c40681e
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-24 10:28:54 -0300

    classes

--- 12_Orientacao_objetos/12_visibilidade/index.php
+++ 12_Orientacao_objetos/12_visibilidade/index.php
@@ -1,38 +1,3 @@
 <?php
 
 
-    class Carro {
-
-        public $ano = 2022;
-        public $cor = "Vermelho";
-        public $modelo = "Sedam";
-        public $marca;
-
-        function ligar(){
-
-            echo "Vrum Vrum...";
-
-        }
-
-        function Andar($m){
-
-            echo "O carro esta Andando<br>"; 
-
-        }
-
-    }
-
-    $civic = new Carro;
-
-    $civic->marca = "Honda";
-
-    echo $civic->marca . "<br>";
-
-    echo $civic->ano . "<br>";
-
-    echo $civic->cor . "<br>";
-
-    echo $civic->modelo . "<br>";
-
-    echo $civic->ligar() . "<br>";
-    echo $civic->Andar(20) . "<br>";
