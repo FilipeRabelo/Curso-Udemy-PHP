354d0d6587eb06e0210aa95fd06bd9bcf12a3316
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-24 12:32:05 -0300

    classes/herancas

--- 12_Orientacao_objetos/13_herança/index.php
+++ 12_Orientacao_objetos/13_herança/index.php
@@ -1,8 +1,67 @@
 <?php
 
+use Programador as GlobalProgramador;
+
     echo "A HERANÇA é o recurso da POO que a possibilidade de uma classe HER METODOS
     E PROPRIEDADE DE OUTRA<br>
     A palavra reservada é EXTENDS<br>
     Exe: class Programador extens Pessoa{
 
-    }";
+    }<hr>";
+
+
+
+    class Humano {
+        public $idade = 29;
+        
+        public function Falar(){
+            echo "Ola mundo <br>";
+        }
+
+        //////////////////////////
+
+        private function Gritar(){
+            echo "PHP é muito bom!!! <br>";
+        }
+
+        public function Acesso_gritar(){
+            $this->Gritar();
+        }
+
+        ////////////////////////
+
+        protected function Falar_baixinho(){  // protected consigo acessar nas classes filhas // private NAO //
+            echo "LAlalalal <br>";
+        }
+
+        public function acesso_falar_baixinho(){
+            $this->Falar_baixinho();
+        }
+
+    }
+
+    class Programador extends Humano{
+
+        public function acesso_falar_baixinho_programador() {
+            $this->Falar_baixinho();
+        }
+
+
+    }
+
+    $filipe = new Humano;
+
+    $filipe->Falar() . "<br>";
+    $filipe->Acesso_gritar() . "<br";
+    $filipe->acesso_falar_baixinho() . "<br>";
+
+    echo $filipe->idade . "<br>";
+
+    echo "<hr>";
+
+    $giulia = new Programador;
+
+    $giulia->Falar();
+    $giulia->Acesso_gritar();
+    $giulia->acesso_falar_baixinho();
+    echo $giulia->idade;
