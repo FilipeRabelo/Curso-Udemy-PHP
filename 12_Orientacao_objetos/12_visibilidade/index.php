af97445882693d052b7af2a2d8ee93fa6ef79b24
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-24 12:13:50 -0300

    classes

--- 12_Orientacao_objetos/12_visibilidade/index.php
+++ 12_Orientacao_objetos/12_visibilidade/index.php
@@ -1,3 +1,75 @@
 <?php
 
+use Car as GlobalCar;
+
+echo "temos tres formatos de visibilidade<br>
+PUBLIC = propriedade ou metodo q pode ser acessado de qualuqer forma<br>
+PROTECTED = Propriedade ou metado pode ser acessada apenas pela classe origem ou as que 
+recebam a mesma heranca;<br>
+PRIVATE = a propriedade ou metado pode ser acessada apenas pela classe q foi craida<hr>
+";
+
+    class Car{
+
+        public $rodas = 4;
+      
+        private $vidros = "Sem pelicula";
+
+        protected $portas = 4;
+
+        public function get_vidro(){
+            return $this->vidros;
+        }
+
+        public function get_portas(){
+            return $this->portas;
+        }
+
+    }
+
+    class Mecanico{
+
+        public function alterar_rodas($carro){
+
+            $carro->rodas = 10;
+
+        }
+
+        public function colocar_pelicula($carro, $pelicula){
+
+            $carro->vidros = $pelicula;
+
+        }
+
+    }
+
+    $carro = new Car;
+
+    echo $carro->rodas;
+
+    echo "<hr>";
+
+    $filipe = new Mecanico;
+
+    $filipe->alterar_rodas($carro);
+    echo $carro->rodas;
+
+    echo "<hr>";
+
+    // Nao pode alterar pq é privado
+    // $filipe->colocar_pelicula($carro, "G20");
+
+    // $carro->pelicular_de_fabrica("G10");
+
+    echo $carro->get_vidro() . "<br>";
+
+    echo $carro->get_portas() . "<br>";
+
+
+
+
+
+
+
+
 
---
+++ 12_Orientacao_objetos/13_herança/index.php
@@ -0,0 +1,8 @@
+<?php
+
+    echo "A HERANÇA é o recurso da POO que a possibilidade de uma classe HER METODOS
+    E PROPRIEDADE DE OUTRA<br>
+    A palavra reservada é EXTENDS<br>
+    Exe: class Programador extens Pessoa{
+
+    }";
