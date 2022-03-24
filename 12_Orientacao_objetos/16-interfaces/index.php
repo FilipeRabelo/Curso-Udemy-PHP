5a580b3085c37b216344e0c8cd7a23186703cf59
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-24 13:37:14 -0300

    classes

---
+++ 12_Orientacao_objetos/16-interfaces/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    echo "As interfaces criam um MODELO de definição de uma classe.<br>
+    Entao toda classe que implementar uma interface, devera implementr tambem duas 
+    propriedades e metados, obrigatoriamente;<br>
+    A palavra reservada é implements;<br>
+    clsd Humano implements Caracteristicas.
+    
+    IMPREMENTAR ===>>  implements   <hr>";
+
