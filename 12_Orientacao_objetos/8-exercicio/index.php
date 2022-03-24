45140fb512f83f3b6e6584d9a58015afe99192d8
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-23 22:05:01 -0300

    classes

--- 12_Orientacao_objetos/8-exercicio/index.php
+++ 12_Orientacao_objetos/8-exercicio/index.php
@@ -5,9 +5,9 @@
         public $nome = "Filipe";
         public $idade =32;
 
-        function Andar(){
+        function Andar($m){
 
-            echo "Esta andando <br>";
+            echo "Eu andei $m metros hoje! <br>";
 
         }
 
@@ -15,30 +15,33 @@
 
     $filipe = new Pessoa;
 
-    echo $filipe->Andar();
+    echo $filipe->Andar(5458);
 
     echo $filipe->nome . "<br>";
 
     echo "Possui: " . $filipe->idade . " anos de idade <br>"  ;
 
 
+
+
+
     echo "<hr>";
+
+
+
+
 
     class Pessoa2{
 
         public $nome = "Giulia";
         public $idade = 6;
 
-        function Andar (){
-
-            echo "Andando... <br>";
-
+        function Andar ($m){
+            echo "A pessoa andou $m metros!";
         }
 
         function Falar(){
-
             echo "Bom dia ..";
-
         }
 
     }
@@ -46,9 +49,9 @@
     $giulia = new Pessoa2;
 
     echo $giulia->Falar() . "<br>";
+    echo $giulia->Andar(1000) . "<br>";
     echo $giulia->nome . "<br>";
     echo $giulia->idade . "<br>";
-    echo $giulia->Andar() . "<br>";
 
 
 ?>
