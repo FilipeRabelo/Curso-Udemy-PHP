c2e23f3135a608feb7eadce2c245d119577fb04a
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-24 22:36:46 -0300

    POO

---
+++ 12_Orientacao_objetos/19_construct/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "Pelos construtores podemos inicializar objetos com valores de 
+    propriedades unicas para cada objeto<br>
+    Passamos como argumanto os valores das propriedades<br>
+    
+    ex: 
+    function__construct($ portas, $ motor, $ teto_solar){}
+    <hr>";
+
+
