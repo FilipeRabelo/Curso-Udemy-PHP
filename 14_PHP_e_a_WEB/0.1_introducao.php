b9f0727485a733b3ddfe7587697cbb466ae11265
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-25 18:33:50 -0300

    php/web

--- 14_PHP_e_a_WEB/0.1_introducao.php
+++ 14_PHP_e_a_WEB/0.1_introducao.php
@@ -2,21 +2,23 @@
 
     <h5>As requisições que enviamos tambem contem metodos, alguns deles são:</h5>
     <p>
-        <span>GET:</span> Solicita a apresentação de um recurso ( ex: Visualização de uma página);<br>
+        <span>GET: RECEBENDO DADOS DO SERVIDOR</span> 
+        Solicita a apresentação de um recurso ( ex: Visualização de uma página);<br>
         <span>GET --> PEGAR / BUSCAR / VISUALIZAR </span>
     </p>
 
     <p>
-        <span>POST:</span> Envio de dados ao servidor (EX: cadastro de usuário);
+        <span>POST: ENVIANDO DADOS PARA O SERVIDOR</span> 
+        Envio de dados ao servidor (EX: cadastro de usuário);<br>
         <span>Enviar as informações;</span>
     </p>
 
     <p>
-        <span>PUT:</span> Atualização de dados;
+        <span>PUT:</span> Atualização de dados; Atualização no banco de dados;
     </p>
 
     <p>
-        <span>DELETE: </span> Remoção de dados;
+        <span>DELETE: </span> Remoção de dados; Edeltar dados no banco de dados;
     </p>
 
     <p>
