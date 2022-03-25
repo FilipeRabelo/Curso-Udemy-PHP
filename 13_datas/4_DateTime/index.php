595e87c103e647c43554d1ec4e1a5c427db6f6f5
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-25 13:00:19 -0300

    date

---
+++ 13_datas/4_DateTime/index.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "O OBJETO DateTime pormite tratar a data como um objeto<br>
+    Podemos passar um parametro que sera a data criada, se NAO passarmos nada a daa sera a ATUAL<br>
+    Usar prit_r para exibir informações.<br>
+    Ex: $ dataAtual = new DateTime();<hr>";
+
+
+    $data_atual = new DateTime();
+
+    print_r($data_atual);
