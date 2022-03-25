24fb002fda4a19213a2cab1d8f244045cf294131
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-25 10:22:22 -0300

    date

--- 13_datas/1_funcao_date/index.php
+++ 13_datas/1_funcao_date/index.php
@@ -3,7 +3,10 @@
     echo "A funcao date recebe um paramentro, que é o FORMATO DA DATA, e este é o 
     primeiro parametro da mesma<br>
     A resposta sera a DATA ATUAL<br>
-    Ex: date('d/m/y'); // day //month // year ";
+    Ex: date('d/m/y'); // day //month // year <hr> ";
 
 
 
+    $d = date('d/m/y'); // day //month // year 
+
+    echo $d;
