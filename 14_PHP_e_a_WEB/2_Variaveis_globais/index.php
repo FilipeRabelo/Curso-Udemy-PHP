5b13983239501b745f8481b4d8aa5b61bcb778fd
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-25 18:47:08 -0300

    php/web

---
+++ 14_PHP_e_a_WEB/2_Variaveis_globais/index.php
@@ -0,0 +1,25 @@
+<h3>Variaveis Globais do PHP</h3>
+
+<p>
+    <span>$_ENV: </span>Variaveis de ambiente
+</p>
+
+<p>
+    <span>$_GET: </span>Parametros que foram enviados por request <span>GET</span>
+</p>
+
+<p>
+    <span>$_POST: </span>Parametros que foram enviados por <span>POST</span>
+</p>
+
+<p>
+    <span>$_COOKIE: </span>Valores de <span>COOKIES</span>
+</p>
+
+<p>
+    <span>$_SERVER: </span>Informações sobre o SERVIDOR
+</p>
+
+<p>
+    <span>$_FILES: </span>Informaçoes sobre os arquivos que vieram por upload
+</p>
