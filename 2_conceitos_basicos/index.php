da04a281e49316c6d4d70e45c3ecb486fe83f2b7
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-26 11:04:13 -0300

    php/web

--- 2_conceitos_basicos/index.php
+++ 2_conceitos_basicos/index.php
@@ -29,10 +29,44 @@
     if (5 > 3) {
         echo "Dentro do if";
     }
+
+    echo "<hr>";
+
     ?>
 
-    <?php
+    <?php   // Construind objeto como construct // ESTUDOS
 
+    class Casa {
+
+        public $sala;
+        public $cozinha;
+        public $banheiro;
+        public $quartos;
+
+
+        public function __construct($sala, $cozinha, $banheiro, $quartos){
+
+            $this->sala = $sala;
+            $this->cozinha = $cozinha;
+            $this->banheiro = $banheiro;
+            $this->quartos = $quartos;
+        }
+
+        public function Mostrar_casa(){
+            echo "Essa casa possui uma $this->sala, $this->cozinha, $this->banheiro e $this->quartos";
+        }
+
+    }
+
+    $Casa_4201 = new Casa("Sala ampla", "Cozinha espaçosa", "2 Banheiros", " 3 quartos");
+
+    $Casa_4201->Mostrar_casa();
+
+    echo "<hr>";
+
+    $casa_4455 = new Casa("2 Salas", "cozinha arejada", "1 Banheiro", "4 Quartos");
+
+    $casa_4455->Mostrar_casa();
 
 
 
