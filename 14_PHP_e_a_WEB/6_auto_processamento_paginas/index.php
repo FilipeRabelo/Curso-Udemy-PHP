b54e36cc4741ab10dddc165316a1019b64aaecc2
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-26 20:42:18 -0300

    php/web

---
+++ 14_PHP_e_a_WEB/6_auto_processamento_paginas/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        body {
+            background-color: aqua;
+        }
+    </style>
+</head>
+
+<body>
+
+    <h3>
+        Podemos criar uma pagina que faz o processamento dos dados e tambem exibe o input de informaçoes <br>
+        Para isso devemos criar um IF() que checa se o metodo de requisição é ou não é um GET ou POST <br>
+        <br>
+        ($_SERVE['REQUEST_METHOD']); <br>
+        <br>
+        Depois criar as duas variações, para cada uma das possibilidades; <br>
+        Ou checar se algum parametro veio pela requisição e então criar as variações.
+        <hr>
+    </h3>
+
+
+</body>
+
+
+</html>
+
+
+<?php
