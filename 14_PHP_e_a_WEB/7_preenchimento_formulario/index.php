09b8a1ad297e70b83932c2d54407360754bc0450
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-26 21:30:03 -0300

    php/web

---
+++ 14_PHP_e_a_WEB/7_preenchimento_formulario/index.php
@@ -0,0 +1,39 @@
+<h3>
+    Podemos preencher o formúlario com dados que vierem da requisição <br>
+    Isso acontece bastante em resultados de busca ou edições de registro <br>
+    Podemos fazer uma checagem de se o dado foi enviado e recebido para a pagina, 
+    e utilizar o echo para exibir no atributo value do input <br>    
+</h3>
+
+
+    <h2>DADOS MOCADOS, eles nao estao no servidor...</h2>
+
+
+<?php
+
+    $usuario = [
+        "nome" => "Filipe",
+        "idade" => 32,
+        "profissao" => "Programador"
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: aqua;
+        }
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
+
