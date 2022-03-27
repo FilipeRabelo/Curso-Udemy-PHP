ad41b8bf9b5db2f3872d57f2f491f9b03447cf98
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-26 21:49:43 -0300

    php/web

--- 14_PHP_e_a_WEB/7_preenchimento_formulario/index.php
+++ 14_PHP_e_a_WEB/7_preenchimento_formulario/index.php
@@ -4,18 +4,30 @@
     Podemos fazer uma checagem de se o dado foi enviado e recebido para a pagina, 
     e utilizar o echo para exibir no atributo value do input <br>    
 </h3>
-
-
-    <h2>DADOS MOCADOS, eles nao estao no servidor...</h2>
+<h2>
+    DADOS MOCADOS, para fazer apenas uma apresentacao grafica do projeto <br>
+    eles nao estao no servidor...
+</h2>
 
 
 <?php
 
     $usuario = [
-        "nome" => "Filipe",
+        "nome" => "Filipe",   // O input ja vai estar com esses dados ///
         "idade" => 32,
         "profissao" => "Programador"
     ];
+
+    if($usuario) {
+
+        $nome = $usuario['nome'];
+        $idade= $usuario["idade"];
+        $profissao = $usuario["profissao"];
+
+    }
+
+
+
 
 ?>
 
@@ -27,12 +39,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-        body{
+        body{          
             background-color: aqua;
         }
     </style>
 </head>
 <body>
+
+        <form action="">
+            <div>
+                <input type="text" name="nome" placeholder="Digite seu nome: " value="<?= $nome ?>"> <!-- Input ja vao estar com os dados -->
+            </div>
+            <div>
+                <input type="text" name="idade" placeholder="Digite seu idade: " value="<?= $idade ?>">
+            </div>
+            <div>
+                <input type="text" name="profissao" placeholder="Digite seu profissao: " value="<?= $profissao ?>">
+            </div>
+            <div>
+                <input type="submit" value="Enviando... ">
+            </div>
+        </form>
     
 </body>
 </html>
