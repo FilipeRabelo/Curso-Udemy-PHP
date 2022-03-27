1e24327fea1ec17881bf4c57323019d0440958c7
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-27 14:28:01 -0300

    php/web

---
+++ 14_PHP_e_a_WEB/13_SSL/index.php
@@ -0,0 +1,9 @@
+<h2>
+    SSL vem de SECURE SOCKETS LAYER <br>
+    O PHP nao se importa muito  e nao tem vantagens sobe o SSL; <br>
+    Porem garatinmos que as requisicoes de dados entre nosso site e servidor estejam mais seguras com a 
+    encriptação de dados <br>
+    Devemos apenas ter cuidado com os formularios, para que sejam enviados para a URL co o HTTPS,  <br>
+    pois alguns servidores bloqueim a conexão HTTP; <hr>
+    
+</h2>
