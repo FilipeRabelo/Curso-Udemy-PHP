b9cfde4fb25edad174b133e4481802682ab3e894
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-28 23:26:36 -0300

    PHP

---
+++ 16_bancoDeDados_MySQL_PHP/1_introdução.php
@@ -0,0 +1,8 @@
+<h1>Introdução</h1>
+<h2>
+    O php tem suporte para mais de 20 bancos de dados; <br>
+    O mais comum a ser utilizado é o MySQL; <br>
+    Há algumas formas de <span>(CONEXÃO)</span> a bancos disponiveis no PHP: <br>
+    PDO (PHP Data Objects) &  MySQLi <br>
+    As duas sao orientadas a objetos    
+</h2>
---
+++ 16_bancoDeDados_MySQL_PHP/2_banco_de_dados_relacional.php
@@ -0,0 +1,13 @@
+<h1>Banco de dados relacionais</h1>
+<h2>
+    
+    Structured Query Language, ou Linguagem de Consulta Estruturada ou SQL, <br>
+    é a linguagem de pesquisa declarativa padrão para banco de dados relacional. <br> 
+    Muitas das características originais do SQL foram inspiradas na álgebra relacional. <br> <hr>
+
+    O banco de dados relacionais tem como caracteristica principal trabalhar com tabelas; <br>
+    O PHP é muito utilizado com DBs relacionais, como o MySQLi; <br>
+    A linguagem para operação com esses bancos é a SQL <br>
+    As intruções em SQL costumam ser escritas em letras maiusculas <br>
+
+</h2>
