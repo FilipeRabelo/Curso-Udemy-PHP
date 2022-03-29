d7dd78b83d8d789996b9c0fff987e15bdcb6152c
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-29 11:09:05 -0300

    PHP

---
+++ 16_bancoDeDados_MySQL_PHP/3_criando_banco_de_dados.php
@@ -0,0 +1,8 @@
+<h2>
+    Podemos criar bancos de dados manualmente em softwares como o phpMyAdin ou SQL <br>
+    Vamos utilizar comandos de QUERY que v~çao servir ao ongo da carreria de progração. <br>
+    Para criar bancos vamos utilizar o: <br>
+    CREATE DATABASE nomedobanco; <br>
+
+
+</h2>
---
+++ 16_bancoDeDados_MySQL_PHP/5_tipos_dados.php
@@ -0,0 +1,9 @@
+PRINCIPAIS TIPOS DE DADOS:
+Os tipos de dados funcionam como os tipos de variáveis, porem em varios “níveis” para melhor PERFORMECE;
+•	VARCHAR: Texto de 0a 65.535 CARACTERES. Ex: Nome, e-mail, endereço ...
+Obs.: podemos limitar o tamanho do VARCHAR 
+•	TEXT: Texto como maximo de 65.535 BYTES. Ex: Texto de um blog...
+•	INT: Números inteiros,
+•	BIGINT: Números inteiros com maior proporção que o INT.
+•	DATE: Data no formato YYYY-MM-DD;
+
