e3e02297e18b1519d9cfba913b8aa5f5c110f14b
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-29 11:53:19 -0300

    PHP/bando de dados

---
+++ 16_bancoDeDados_MySQL_PHP/6_criando_tabelas.php
@@ -0,0 +1,8 @@
+CRIANDO TABELAS
+As tabelas ficam dentro dos bancos, e os dados ficam dentro das tabelas, ou seja, é parte fundamental do banco relacional;
+Podemos criar tabelas facilmente por SQL, o comando é: 
+
+CREATE TABLE nome (
+	Coluna   tipoDeDado,
+	Coluna2  tipoDeDado
+);
---
+++ 16_bancoDeDados_MySQL_PHP/7_alterando_tabelas.php
@@ -0,0 +1,7 @@
+ALTERANDO TABELAS
+Podemos alterar uma tabela já criada com:
+
+ALTER TABLE nomeDaTabela
+ADD / DROP / MODIFY COLUMN nomeDaTabela;
+
+Adicionar tabela / remover tabela / modificar tabela
