25e3c635144243d968e765ebcd44290484fe06fb
Filipe Rabelo F Lana <filipe.rabelolana@hotmail.com>
2022-03-30 23:51:38 -0300

    PHP_MySQL

--- 17_PHP_MySQL/6_select_dados_mysqli/index.php
+++ 17_PHP_MySQL/6_select_dados_mysqli/index.php
@@ -1,10 +1,10 @@
-<h3>
+<!-- <h3>
   SELECIONANDO DADOS COM MYSQLI
   Para resgatar dados com MySQLi vamos utilizar a query do SQL -> QUERY,
   A instrução para inserir dados é SELECT,
   Vamos inserir o método query em uma variavel que é onde receberemos os resultados.
   Como método fetch_assoc, transformamos os resultados em um ARRAY;
-</h3>
+</h3> -->
 
 <?php
 
@@ -14,3 +14,21 @@
   $bd   = "cursophp";
 
   $conn = new mysqli($host, $user, $pass, $bd);
+
+
+  $q = "SELECT * FROM itens";
+
+  $resultado = $conn->query($q);
+
+  $conn->close();
+
+  // UM RESULTADO //
+  $item = $resultado->fetch_assoc();
+
+  print_r($item);
+
+  // TODOS OS RESULTADOS // 
+  $itens = $resultado->fetch_all();
+
+  print_r($itens);
+   
